6cdd360ef8e461e2b239f87fed62623c48824c5b
Stephanie Wang <steph17@stanford.edu>
2016-04-27 21:32:47 -0700

    Fixed minor syntax

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = require('./routes');
 var app = express();
-var airtableConfig = require('./secrets');
+var airtableConfig = require('./secret');
 var Airtable = require('airtable');
+
 //Configure the Airtable API
 Airtable.configure({
 	endpointUrl: 'http://localhost:5000', 
-	apikey: airtableConfig["AIRTABLE_API_KEY"];
+	apikey: airtableConfig["AIRTABLE_API_KEY"]
 });
 var base = Airtable.base('appfroa8YN4yjSWIk');
 
@@ -15,6 +16,26 @@ app.use('/', router);
 app.use(express.static('public'));
 app.get('/', function (req, res) {
 	res.sendFile( __dirname + "/views" + "/index.jade" );
+});
+
+
+//To list records in Beneficiaries. 
+base('Beneficiaries').select({
+	//Selecting the first 3 records in Main View:
+	maxRecords: 3,
+	view: "Main View"
+}).eachPage(function page(records, fetchNextPage) {
+
+	//This function will get called for each page of records.
+	records.forEach(function(record) {
+		console.log('Retreived', record.get('Name'));
+	});
+	fetchNextPage();
+
+}, function done(error) {
+	if(error) {
+		console.log(error);
+	}
 });
 
 
