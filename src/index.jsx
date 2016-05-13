110557e051959aa455c7e0fbfd09033f558a2bc6
Bronson Duran <brons@outlook.com>
2016-05-13 11:57:43 -0700

    app structure

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Home = require('./Home.jsx');
-var Routes = require('./routes.jsx');
-
-
-ReactDOM.render((Routes), document.getElementById('container'));
